706efe30fbc01212ee66fb6b097180c511593403
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-06-08 17:13:04 -0400

    adding a parser test

--- test/parser_test.js
+++ test/parser_test.js
@@ -1,1 +1,26 @@
+var expect = require('chai').expect;
+var tokenize = require('../lib/lexer').tokenize;
+var parser = require('../lib/parser');
 
+
+describe('parser', function() {
+  console.log(parser);
+  var parse = parser.parse;
+
+  describe('#parse()', function() {
+    it('should parse a variable assigned to a string', function() {
+      var input = 'ب = "مرحبا الي الريكرس سنتر"';
+      var expectedTree = JSON.parse('{"type":"Program","val":[{"type":"AssignmentExpression","val":{"type":"AtomAssignment","val":[{"type":"IDENTIFIER","val":"ب"},{"type":"STRING","val":"\'مرحبا الي الريكرس سنتر\'"}]}}]}');
+      var result = parse(tokenize(input));
+      console.log(result);
+      expect(result).to.deep.equal(expectedTree);
+    });
+  });
+});
+
+// expected = [
+//     ["IDENTIFIER","ب"],
+//     ["=","="],
+//     ["STRING","\"مرحبا الي الريكرس سنتر\""],
+//     ["\n","\n"],["\n","\n"]
+//   ]
