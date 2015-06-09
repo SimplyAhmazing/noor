5164bf99bca1f46fb7981c60bc89502ecb9b4b9b
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-06-09 16:36:04 -0400

    sets up travis CI for running tests

---
+++ test/interpreter_test.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var interpreter = require('../lib/interpreter');
+var parse = require('../lib/parser').parse;
+var scope = require('../lib/scope');
+var tokenize = require('../lib/lexer').tokenize;
+var utils = require('../lib/utils');
+
+
+describe('interpeter', function() {
+  var interpret = interpreter.interpret;
+
+  describe('#interpret()', function() {
+    it('should evaluate an addition expression between 2 variables', function() {
+      var input = 'ك = ٣٠' + '\n' +'ن = ٢٠' + '\n' + 'اطبع (ك + ن)' + '\n';
+      var expected = 50;
+
+      var result = null;
+      // TODO: move this 
+      function stdout(text) {
+        result = text;
+      }
+      var scopeObj = scope.createScope(null, stdout);
+      interpret(parse(tokenize(input)), scopeObj);
+
+      expect(result).to.equal(expected);
+    });
+  });
+});
