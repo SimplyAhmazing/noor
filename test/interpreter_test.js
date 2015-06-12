c32bcbf5946af6465e2223b0bd6b801e4faf0f40
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-06-12 16:41:32 -0400

    moves test out of array and places them into individual test functions w/expected results

--- test/interpreter_test.js
+++ test/interpreter_test.js
@@ -1,29 +1,29 @@
-var expect = require('chai').expect;
-var interpreter = require('../lib/interpreter');
-var parse = require('../lib/parser').parse;
-var scope = require('../lib/scope');
-var tokenize = require('../lib/lexer').tokenize;
-var utils = require('../lib/utils');
-// var testUtils = require('../test/test_utils');
-
-
-describe('interpeter', function() {
-  var interpret = interpreter.interpret;
-
-  describe('#interpret()', function() {
-    it('should evaluate an addition expression between 2 variables', function() {
-      var input = 'ك = ٣٠' + '\n' +'ن = ٢٠' + '\n' + 'اطبع (ك + ن)' + '\n';
-      var expected = 50;
-
-      var result = null;
-      // TODO: move this to test util module
-      function stdout(text) {
-        result = text;
-      }
-      var scopeObj = scope.createScope(null, stdout);
-      interpret(parse(tokenize(input)), scopeObj);
-
-      expect(result).to.equal(expected);
-    });
-  });
-});
+// var expect = require('chai').expect;
+// var interpreter = require('../lib/interpreter');
+// var parse = require('../lib/parser').parse;
+// var scope = require('../lib/scope');
+// var tokenize = require('../lib/lexer').tokenize;
+// var utils = require('../lib/utils');
+// // var testUtils = require('../test/test_utils');
+// 
+// 
+// describe('interpeter', function() {
+//   var interpret = interpreter.interpret;
+// 
+//   describe('#interpret()', function() {
+//     it('should evaluate an addition expression between 2 variables', function() {
+//       var input = 'ك = ٣٠' + '\n' +'ن = ٢٠' + '\n' + 'اطبع (ك + ن)' + '\n';
+//       var expected = 50;
+// 
+//       var result = null;
+//       // TODO: move this to test util module
+//       function stdout(text) {
+//         result = text;
+//       }
+//       var scopeObj = scope.createScope(null, stdout);
+//       interpret(parse(tokenize(input)), scopeObj);
+// 
+//       expect(result).to.equal(expected);
+//     });
+//   });
+// });
--- test/pegjs_parser_test.js
+++ test/pegjs_parser_test.js
@@ -2,28 +2,13 @@ var parse = require('../lib/peg-parser');
 var expect = require('chai').expect;
 var d = require('../lib/utils').logger('parseTests');
 
-// 
-// var inputs    = [
-//   '١٢\n',
-//   '١٢٣٤٥٦٧٨٩٠\n',
-//   '"ن"\n',
-//   'ب = ١٠\n',
-//   'ب = "مودت"\n',
-//   'اطبع ١٠\n',
-//   'اطبع ١٠ "كلم"\n',
-//   'اطبع "كلم"\n',
-//   'اطبع سوال\n',
-//   'ب = ١٠\n',
-//   '٩+٧\n',
-//   '٧\n',
-//   'ب = ٧+٩\n',
-//   '١\n',
-//   '٢١\n',
-//   'ب = ١+٢+٣\n',
-//   'ب + ي\n',
-//   // '',
-// ];
-// 
+var inputs    = [
+  'ب = ٧+٩\n',
+  'ب = ١+٢+٣\n',
+  'ب + ي\n',
+  // '',
+];
+
 // inputs.map(function(input) {
 //   d(input);
 //   d(JSON.stringify(parse(input), null, 4));
@@ -37,9 +22,149 @@ describe('peg-parser', function() {
     it('should parse an operator expression like "a + b"', function() {
       // var input = 'ب = "مرحبا الي الريكرس سنتر"';
       var input = 'ب + ي\n';
-      var expectedString = {"type":"Program","val":[{"type":"OperatorExpression","val":[{"type":"Identifier","val":"ب"},{"type":"AdditionOperator","val":"+"},{"type":"InvocationExpression","val":[{"type":"Identifier","val":"ي"}]}]}]};
+      var expectedString = {
+        "type":"Program",
+        "val":[
+          {"type":"OperatorExpression",
+            "val":[
+              {"type":"Identifier","val":"ب"},
+              {"type":"AdditionOperator","val":"+"},
+              {
+                "type":"InvocationExpression",
+                "val":[
+                  {"type":"Identifier","val":"ي"}
+                ]
+              }
+            ]
+        }]
+      };
       var result = parse(input);
-      expect(result).to.equal(expectedString);
+      expect(result).to.deep.equal(expectedString);
     });
+
+    it('should parse standalone integers and strings', function() {
+      var inputToOutputResultMap = [
+        ['١٢\n', {"type":"Program","val":[{"type": "Integer", "val": 12}]}],
+        ['١٢٣٤٥٦٧٨٩٠\n', {"type":"Program","val":[{"type":"Integer","val":1234567890}]}],
+        ['"ن"\n', {"type":"Program","val":[{"type":"String","val":"ن"}]}],
+      ];
+      inputToOutputResultMap.forEach(function(item) {
+        expect(parse(item[0])).to.deep.equal(item[1]);
+      });
+    });
+
+    it('should parse assigning a variable to a string or a number', function() {
+      var inputToOutputResultMap = [
+        ['ب = ١٠\n', {"type":"Program","val":[{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ب"},{"type":"Integer","val":10}]}]}],
+        ['ب = "مودت"\n', {"type":"Program","val":[{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ب"},{"type":"String","val":"مودت"}]}]}],
+      ];
+      inputToOutputResultMap.forEach(function(item) {
+        expect(parse(item[0])).to.deep.equal(item[1]);
+      });
+    });
+
+    it('should parse calling a ftn with an int arg', function() {
+      var input = 'اطبع ١٠\n';
+      var expected = {
+        "type":"Program",
+        "val":[
+          {"type":"InvocationExpression",
+            "val":[{"type":"Identifier","val":"اطبع"},{"type":"Integer","val":10}]}]};
+      expect(parse(input)).to.deep.equal(expected);
+    });
+
+    it('should parse calling a ftn with 2 arguments', function() {
+      var input = 'اطبع ١٠ "كلم"\n';
+      var expected = {
+        "type":"Program",
+        "val":[
+          {"type":"InvocationExpression",
+            "val":[
+              {"type":"Identifier","val":"اطبع"},
+              {"type":"Integer","val":10},
+              {"type": "String", "val": "كلم"}
+            ]}]};
+
+      // console.log(JSON.stringify(parse(input)));
+      expect(parse(input)).to.deep.equal(expected);
+    });
+
+    it('should parse calling a ftn with an string argument', function() {
+      var input = 'اطبع "كلم"\n';
+      var expected = {
+        "type":"Program",
+        "val":[
+          {"type":"InvocationExpression",
+            "val":[{"type":"Identifier","val":"اطبع"},{"type":"String","val":"كلم"}]}]};
+      expect(parse(input)).to.deep.equal(expected);
+    });
+
+    it('should parse calling a ftn with an int arg', function() {
+      var input = 'اطبع "كلم"\n';
+      var expected = {
+        "type":"Program",
+        "val":[
+          {"type":"InvocationExpression",
+            "val":[{"type":"Identifier","val":"اطبع"},{"type":"String","val":"كلم"}]}]};
+      expect(parse(input)).to.deep.equal(expected);
+    });
+
+    it('should parse calling a ftn with an identifier (variable or 0-arg ftn)', function() {
+      var input = 'اطبع سوال\n';
+      var expected = {
+        "type":"Program",
+        "val":[
+          {"type":"InvocationExpression",
+            "val":[
+              {"type":"Identifier","val":"اطبع"},
+              {"type":"InvocationExpression",
+               "val":[{"type":"Identifier","val":"سوال"}]}]}]};
+
+      expect(parse(input)).to.deep.equal(expected);
+    });
+
+    it('should parse calling a ftn with an identifier (variable or 0-arg ftn)', function() {
+      var input = 'اطبع سوال\n';
+      var expected ={
+        "type":"Program",
+        "val":[
+          {"type":"InvocationExpression",
+           "val":[
+            {"type":"Identifier",
+             "val":"اطبع"
+            },
+            {"type":"InvocationExpression",
+              "val":[
+                {"type":"Identifier",
+                "val":"سوال"
+              }]}
+          ]
+        }]
+      };
+      expect(parse(input)).to.deep.equal(expected);
+    });
+
+    it('should parse an arithmetic operation between 2 integers', function() {
+
+      var input = '٩+٧\n';
+      var expected = {
+        "type":"Program",
+        "val":[
+          {"type":"OperatorExpression",
+           "val":[
+            {"type":"Integer",
+             "val":9
+            },
+            {"type":"AdditionOperator",
+              "val":"+"
+            },
+            {"type":"Integer",
+              "val":7
+            }]
+        }]
+      };
+      expect(parse(input)).to.deep.equal(expected);
+    });
+    // console.log(JSON.stringify(parse(input)));
   });
 });
