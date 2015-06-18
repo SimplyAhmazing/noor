eab2a016b0db2149d03dba955652e493b11d6931
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-06-17 20:03:04 -0400

    removes duplicate test module; cleaning up test

--- test/parser/simple-expressions.js
+++ test/parser/simple-expressions.js
@@ -201,17 +201,6 @@ describe('peg-parser', function() {
       expect(parse(input)).to.deep.equal(expected);
     });
 
-    it('should an if-statement w/o else clause (with an Atom as the predicate)', function() {
-      var input = 'لو ٥ سوي \nك = ١٠٠\nت=٥\nبس';
-      var expected ={"type":"Program","val":[{"type":"IfElseExpression","val":[{"type":"Integer","val":5},[{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ك"},{"type":"Integer","val":100}]},{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ت"},{"type":"Integer","val":5}]}],null]}]};
-      expect(parse(input)).to.deep.equal(expected);
-    });
-
-    it('should an if-statement with an else clause (with an Atom as the predicate)', function() {
-      var input = 'لو ٥ سوي \nك = ١٠٠\nت=٥\nوالا\n\nي=٧٧٧\n\nبس';
-      var expected ={"type":"Program","val":[{"type":"IfElseExpression","val":[{"type":"Integer","val":5},[{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ك"},{"type":"Integer","val":100}]},{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ت"},{"type":"Integer","val":5}]}],[{"type":"AssignmentExpression","val":[{"type":"Identifier","val":"ي"},{"type":"Integer","val":777}]}]]}]};
-      expect(parse(input)).to.deep.equal(expected);
-    });
     // d(console.log(JSON.stringify(parse(input))), null, 4);
     //
   });
