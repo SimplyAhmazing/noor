a6f34ffb87e56eaf12e5eba6d01239430c5f9d3d
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-06-18 11:45:36 -0400

    sets up test for a calling a function w/ multiple comma separated arguments

--- test/parser/simple-expressions.js
+++ test/parser/simple-expressions.js
@@ -49,62 +49,6 @@ describe('peg-parser-simple-expressions', function() {
       });
     });
 
-    it('should parse calling a ftn with an int arg', function() {
-      var input = 'اطبع ١٠\n';
-      var expected = {
-        "type":"Program",
-        "val":[
-          {"type":"InvocationExpression",
-            "val":[{"type":"Identifier","val":"اطبع"},{"type":"Integer","val":10}]}]};
-      expect(parse(input)).to.deep.equal(expected);
-    });
-
-    it('should parse calling a ftn with 2 arguments', function() {
-      var input = 'اطبع ١٠ "كلم"\n';
-      var expected = {
-        "type":"Program",
-        "val":[
-          {"type":"InvocationExpression",
-            "val":[
-              {"type":"Identifier","val":"اطبع"},
-              {"type":"Integer","val":10},
-              {"type": "String", "val": "كلم"}
-            ]}]};
-
-      // console.log(JSON.stringify(parse(input)));
-      expect(parse(input)).to.deep.equal(expected);
-    });
-
-    it('should parse calling a ftn with a string argument', function() {
-      var input = 'اطبع "كلم"\n';
-      var expected = {
-        "type":"Program",
-        "val":[
-          {"type":"InvocationExpression",
-            "val":[{"type":"Identifier","val":"اطبع"},{"type":"String","val":"كلم"}]}]};
-      expect(parse(input)).to.deep.equal(expected);
-    });
-
-    it('should not parse an expression w/no trailing line and should throw an Exception', function() {
-      var input = 'اطبع "كلم"';
-      var expected = 'Expected Expressions or end of input but "\\u0627" found.';
-      expect(function (){parse(input)}).to.throw(expected);
-    });
-
-    it('should parse calling a ftn with an identifier (variable or 0-arg ftn)', function() {
-      var input = 'اطبع سوال\n';
-      var expected = {
-        "type":"Program",
-        "val":[
-          {"type":"InvocationExpression",
-            "val":[
-              {"type":"Identifier","val":"اطبع"},
-              {"type":"InvocationExpression",
-               "val":[{"type":"Identifier","val":"سوال"}]}]}]};
-
-      expect(parse(input)).to.deep.equal(expected);
-    });
-
     it('should parse an arithmetic operation between 2 integers', function() {
       var input = '٩+٧\n';
       var expected = {
