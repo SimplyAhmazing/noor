59700af4538a9e88c90ea3b15648840dc92e8ec4
Ahmed Abdalla <aabdalla@amplify-nation.com>
2015-06-23 17:15:04 -0400

    adds test for fib function program interpretation

--- test/interpreter/functions.js
+++ test/interpreter/functions.js
@@ -9,20 +9,34 @@ var testUtils = require('../test_utils');
 describe('interpeter', function() {
   var interpret = interpreter.interpret;
 
-  describe('#interpret()', function() {
-    // it('should evaluate a function declaration', function() {
-    //   var input = '';
-    //   var expected = 50;
+  describe('#interpret-functions()', function() {
+    it('should evaluate a function declaration', function() {
+      var input = 'امر عامل فب (ن) ليسوي\n' +
+                  'د = ٠\n' +
+                  'لو ن < ٢ سوي\n' +
+                  'د = ن\n' +
+                  'والا\n' +
+                  'ش = ن - ١\n' +
+                  'س = ن - ٢\n' +
+                  'م = فب(ش)\n' +
+                  'ك = فب(س)\n' +
+                  'د = م + ك\n' +
+                  'بس\n' +
+                  'د\n' +
+                  'بس\n' +
+                  '\n' +
+                  'اطبع (فب(١٠))\n';
+      var expected = 55;
 
-    //   var result = null;
-    //   // TODO: move this to test util module
-    //   function stdout(text) {
-    //     result = text;
-    //   }
-    //   var scopeObj = scope.createScope(null, stdout);
-    //   interpret(parse(input), scopeObj);
+      var result = null;
+      // TODO: move this to test util module
+      function stdout(text) {
+        result = text;
+      }
+      var scopeObj = scope.createScope(null, stdout);
+      interpret(parse(input), scopeObj);
 
-    //   expect(result).to.equal(expected);
-    // });
+      expect(result).to.equal(expected);
+    });
   });
 });
